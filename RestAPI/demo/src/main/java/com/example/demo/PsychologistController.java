b047ee3eb78a24907b42aa922b1aeff8171d55dd
Max Nedobezhkin <firkis@yandex.ru>
2022-06-09 14:57:04 +0700

    Сделал limit and offset

--- RestAPI/demo/src/main/java/com/example/demo/PsychologistController.java
+++ RestAPI/demo/src/main/java/com/example/demo/PsychologistController.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,15 +17,29 @@ public class PsychologistController {
 	
 	private final PsychologistService psychologistService;
 	
+	
 	@Autowired
 	public PsychologistController(PsychologistService psychologistService) {
 		this.psychologistService = psychologistService;
 	}
 	
 	@GetMapping
-	public List<Psychologist> getPsychologist() {
-		return psychologistService.getPsychologist();
+	public List<Psychologist> getPsychologist(@RequestParam(required = false) Integer limit, @RequestParam(required = false) Integer offset) {
+		int limitValue = 25;
+		int offsetValue = 0;
+		
+		if (limit != null) {
+			limitValue = limit.intValue();
+		}
+		
+		if (offset != null) {
+			offsetValue = offset.intValue();
+		}
+		System.out.println(limitValue + " " + offsetValue);
+		return psychologistService.getPsychologist(limitValue, offsetValue);
 	}
+	
+
 	
 	@PostMapping
 	private void registerPsychologist(@RequestBody Psychologist psychologist) {
