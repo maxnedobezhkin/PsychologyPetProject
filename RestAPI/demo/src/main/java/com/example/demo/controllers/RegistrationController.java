25d6c5c48373b883127bd3a7010e01b5ef543dba
max <firkis@yandex.ru>
2022-06-27 17:32:57 +0700

    Сделал регистрацию докторов

--- RestAPI/demo/src/main/java/com/example/demo/controllers/RegistrationController.java
+++ RestAPI/demo/src/main/java/com/example/demo/controllers/RegistrationController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entities.Psychologist;
 import com.example.demo.entities.PsychologistRegistrationData;
+import com.example.demo.service.PsychologistService;
 import com.example.demo.service.RegistrationService;
 
 @RestController
@@ -17,12 +19,24 @@ public class RegistrationController {
 	private RegistrationService registrationService;
 	
 	@Autowired
+	private PsychologistService psychologistService;
+	
+	@Autowired
 	public RegistrationController(RegistrationService registrationService) {
 		this.registrationService = registrationService;
 	}
 	
 	@PostMapping("/psychologist")
 	private void registrationPsychologist(@RequestBody PsychologistRegistrationData psychologistRegistrationData) {
+		Psychologist psychologist;
+		if (psychologistService.findPsychologistById(psychologistRegistrationData.getId()).isPresent()) {
+			psychologist = psychologistService.findPsychologistById(psychologistRegistrationData.getId()).get();
+		} else {
+			throw new NullPointerException("no psychologist");
+		}
+		if (psychologist != null) {
+			psychologistRegistrationData.setPsychologist(psychologist);
+		}
 		registrationService.addPsychologist(psychologistRegistrationData);
 	}
 }
